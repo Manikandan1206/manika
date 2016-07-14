4a92e25abaf57a2d87878dd780ac68350519fa8b
Manikandan1206 <manisivakumar12@gmail.com>
2016-07-14 09:30:14 +0530

    Create divarr.java

---
+++ divarr.java
@@ -0,0 +1,45 @@
+import java.util.*;
+public class divarr {
+	public static void main(String args[]){
+		Scanner in=new Scanner(System.in);
+		System.out.println("Enter the array size:");
+		int size=in.nextInt();
+		int arr[]=new int[size];
+		System.out.println("Enter the Array Elements:");
+		for(int i=0;i<size;i++){
+			arr[i]=in.nextInt();}
+		int rem=0;
+		int temp=size/2;
+		if(size%2!=0){
+			int n=size%2;
+			rem=n+temp;}
+		else{
+			rem=temp;}
+		int result=0;int h=0;
+		for(int i=0;i<rem;i++){
+			result+=arr[i];}
+			h=result/rem;
+			int p=0;int t=0;
+		for(int i=arr.length-1;i>temp;i--){
+			p+=arr[i];}
+			t=p/temp;
+			int g=0;
+		if(h==t){
+			System.out.println("The 2dimensional array is");
+			int s[][]=new int[2][rem];int u=0;
+			System.out.print("{{");
+			for(int i=0;i<rem;i++){
+				s[u][i]=arr[i];
+				System.out.print(s[u][i]+" ");
+				g++;}
+			System.out.print("},{");
+			u++;
+			for(int i=0;i<temp;i++){
+				s[u][i]=arr[g];
+				g++;
+				System.out.print(s[u][i]+" ");}
+			System.out.println("}}");}
+		else{
+			System.out.println("Not Possible");}}}
+
+
